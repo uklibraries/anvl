b3024763747b586ca46494a32b1d1825153c173d
Chris Beer <cabeer@stanford.edu>
2012-08-15 21:42:38 -0700

    standardize spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+
 if ENV['COVERAGE'] and RUBY_VERSION =~ /^1.9/
   require 'simplecov'
-  require 'simplecov-rcov'
-
-  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
   SimpleCov.start
 end
 
-require 'rubygems'
-require 'bundler/setup'
 require 'rspec'
 require 'anvl'
+
+RSpec.configure do |config|
+
+end
